88d530bc3e1bf19495826c89c0341ddd45916c5f
Pushpendra Pal <pushpendrapal1254@gmail.com>
2026-08-20 23:47:59 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,27 @@
+// 1st solution
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer, Double> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            map.put(position[i], (double)(target-position[i])/speed[i]);
+            st.push(i);
+        }
+        
+        Arrays.sort(position);
+
+        int count = 0;
+        while(!st.isEmpty()){
+            int top = st.pop();
+            while(!st.isEmpty() && map.get(position[top]) >= map.get(position[st.peek()])){
+                st.pop();
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
