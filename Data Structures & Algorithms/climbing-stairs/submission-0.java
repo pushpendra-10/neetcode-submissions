46f99efa8e43160124897b5f91f98343763a3992
Pushpendra Pal <pushpendrapal1254@gmail.com>
2026-08-21 00:07:16 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int stepCount(int n, int[] dp){
+        if(dp[n] != -1) return dp[n];
+        if(n == 0 || n == 1) return 1;
+        return dp[n] = stepCount(n-1, dp)+stepCount(n-2, dp);
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return stepCount(n, dp);
+    }
+}
