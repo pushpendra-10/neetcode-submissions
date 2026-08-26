2b5c802a563e1ba1780c394684cbcae9ec96e457
Pushpendra Pal <pushpendrapal1254@gmail.com>
2026-08-26 23:50:49 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int profit = 0;
+        int buy = prices[0];
+        for(int i=1; i<n; i++){
+            if(prices[i] < buy) buy = prices[i];
+            else{
+                profit = Math.max(profit, prices[i] - buy);
+            }
+        }
+        return profit;
+    }
+}
